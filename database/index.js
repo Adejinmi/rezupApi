1ea6eacebac9fa2f81646646b1df17de8ae7448e
samuel <adejinmisamuel1@gmail.com>
2023-10-20 10:22:08 +0100

    lasttime

--- database/index.js
+++ database/index.js
@@ -2,7 +2,4 @@ const mongoose = require ('mongoose')
 const DB = process.env.DB
 
 
-const username = process.env.USER
-const password = process.env.PASSWORD
-
-mongoose.connect(DB)
+mongoose.connect(`${DB}`)
