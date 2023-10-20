f4d5cc3b97a93fc74398a8cd8d93f543e4bf5727
samuel <adejinmisamuel1@gmail.com>
2023-10-20 08:49:52 +0100

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-require('./src/database');
+require('./database');
 app.use(express.json());
 const createClass = require('./routes/createClass')
 const saveAttendance = require('./routes/saveAttendance')
